a40d4f79c1630027da42e05310d9304fa24d9d0b
Atsushi Watanabe <atsushi.w@ieee.org>
2020-07-07 21:32:46 +0900

    Add Parameters/SetParamters to RTPSender/RTPReceiver
    Based on WebAPI's RTCRtpReceiver/RTCRtpSender.
    
    RTPParameters represents encoding/decoding related information
    and RTPSender.SetParameters() will be automatically called by
    PeerConnection according to the result of SDP based negotiation.

--- webrtc/track.go
+++ webrtc/track.go
@@ -21,13 +21,32 @@ type RemoteTrack interface {
 	ReadRTP() (*rtp.Packet, error)
 }
 
+// RTPParameters represents RTCRtpParameters which contains information about
+// how the RTC data is to be encoded/decoded.
+//
+// ref: https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpSendParameters
+type RTPParameters struct {
+	SSRC        uint32
+	PayloadType uint16
+	Encodings   []*webrtc.RTPCodec
+}
+
 // RTPSender represents RTCRtpSender.
 type RTPSender interface {
 	ReplaceTrack(LocalTrack) error
+
+	// Parameters returns information about how the data is to be encoded.
+	Parameters() RTPParameters
+	// SetParameters sets information about how the data is to be encoded.
+	// This will be called by PeerConnection according to the result of
+	// SDP based negotiation.
+	SetParameters(RTPParameters) error
 }
 
 // RTPReceiver represents RTCRtpReceiver.
 type RTPReceiver interface {
+	// Parameters returns information about how the data is to be decoded.
+	Parameters() RTPParameters
 }
 
 // RTPTransceiver represents RTCRtpTransceiver.
