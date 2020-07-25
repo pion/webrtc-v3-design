195d58733ce7127a6a0aa669cbe420c85c42ed7a
Atsushi Watanabe <atsushi.w@ieee.org>
2020-07-25 15:29:54 +0900

    Add PeerConnection.AddTransceiver methods

--- webrtc/go.mod
+++ webrtc/go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/pion/webrtc-v3-design/rtpengine v0.0.0-00010101000000-000000000000
 	github.com/pion/webrtc/v2 v2.2.21
+	github.com/pion/webrtc/v3 v3.0.0-20200724080143-6ee528d349a5
 )
 
 replace github.com/pion/webrtc-v3-design/rtpengine => ../rtpengine
---
+++ webrtc/peerconnection.go
@@ -0,0 +1,62 @@
+package webrtc
+
+import (
+	"github.com/pion/webrtc/v3"
+)
+
+// PeerConnection represents RTCPeerConnection.
+type PeerConnection interface {
+	// AddTrack adds a new LocalTrack which will transmit media to the other peer.
+	//
+	// ref: ttps://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTrack
+	AddTrack(LocalTrack) (RTPSender, error)
+	// AddTransceiverFromTrack creates a new RTPTransceiver from Track
+	// and register it to the PeerConnection.
+	// RTPTransceiver represents a bidirectional stream.
+	//
+	// ref: https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTransceiver
+	AddTransceiverFromTrack(Track, ...RTPTransceiverInit) (RTPTransceiver, error)
+	// AddTransceiverFromTrack creates a new RTPTransceiver from RTPCodecType
+	// and register it to the PeerConnection.
+	// RTPTransceiver represents a bidirectional stream.
+	//
+	// ref: https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTransceiver
+	AddTransceiverFromKind(webrtc.RTPCodecType, ...RTPTransceiverInit) (RTPTransceiver, error)
+}
+
+// RTPTransceiverInitDict represents RTCRtpTransceiverInit dictionary.
+type RTPTransceiverInitDict struct {
+	Direction     webrtc.RTPTransceiverDirection
+	SendEncodings []RTPParameters
+}
+
+// RTPTransceiverInit is a functional option type to set RTPTransceiverInitDict to AddTransceiverFromKind/Track.
+type RTPTransceiverInit func(*RTPTransceiverInitDict)
+
+// RTPTransceiverInitSendonly sets RTPTransceriver direction to sendonly.
+func RTPTransceiverInitSendonly() RTPTransceiverInit {
+	return func(d *RTPTransceiverInitDict) {
+		d.Direction = webrtc.RTPTransceiverDirectionSendonly
+	}
+}
+
+// RTPTransceiverInitSendrecv sets RTPTransceriver direction to sendrecv.
+func RTPTransceiverInitSendrecv() RTPTransceiverInit {
+	return func(d *RTPTransceiverInitDict) {
+		d.Direction = webrtc.RTPTransceiverDirectionSendrecv
+	}
+}
+
+// RTPTransceiverInitRecvonly sets RTPTransceriver direction to recvonly.
+func RTPTransceiverInitRecvonly() RTPTransceiverInit {
+	return func(d *RTPTransceiverInitDict) {
+		d.Direction = webrtc.RTPTransceiverDirectionRecvonly
+	}
+}
+
+// RTPTransceiverInitSendEncodings sets a list of encodings to allow when sending RTP media.
+func RTPTransceiverInitSendEncodings(encodings ...RTPParameters) RTPTransceiverInit {
+	return func(d *RTPTransceiverInitDict) {
+		d.SendEncodings = encodings
+	}
+}
