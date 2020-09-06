1b969cdbc158ea2cf383d7fbabe15db13443dabd
Lukas Herman <lherman.cs@gmail.com>
2020-09-05 21:02:45 -0700

    Add Stop to TrackBase

--- webrtc/track.go
+++ webrtc/track.go
@@ -7,6 +7,7 @@ import (
 // TrackBase represents common MediaStreamTrack functionality of LocalTrack and RemoteTrack.
 type TrackBase interface {
 	ID() string
+	Stop() error
 }
 
 // LocalRTPTrack represents MediaStreamTrack which is fed by the local stream source.
