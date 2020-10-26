4fa9dfbe3a92ff35e94484ddf8bdc461fa0d19a3
Adam Kiss <masterada@gmail.com>
2020-10-26 18:58:27 +0100

    interceptor concept with transportcc example

--- interceptor/interceptor2.go
+++ interceptor/interceptor2.go
@@ -2,6 +2,7 @@ package interceptor
 
 import (
 	"context"
+	"io"
 
 	"github.com/pion/rtcp"
 	"github.com/pion/rtp"
@@ -10,7 +11,6 @@ import (
 
 type Interceptor2 interface {
 	Intercept(context.Context, *webrtc.PeerConnection, ReadWriter) ReadWriter
-	Delete(context.Context, *webrtc.PeerConnection)
 }
 
 // Reader is an interface to handle incoming RTP stream.
@@ -19,16 +19,13 @@ type ReadWriter interface {
 	WriteRTP(context.Context, *rtp.Packet, map[interface{}]interface{}) error
 	ReadRTCP(context.Context) ([]rtcp.Packet, error)
 	WriteRTCP(context.Context, []rtcp.Packet) error
+	io.Closer
 }
 
 type ExampleInterceptor struct{}
 
 func (d *ExampleInterceptor) Intercept(ctx context.Context, connection *webrtc.PeerConnection, readWriter ReadWriter) ReadWriter {
 	return &ExampleReadWriter{readWriter}
-}
-
-func (d *ExampleInterceptor) Delete(ctx context.Context, connection *webrtc.PeerConnection) {
-	// do nothing
 }
 
 type ExampleReadWriter struct {
